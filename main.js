090ab40c0068d52ccb40ca39c2701c02d992ef52
android8624 <9704re@gmail.com>
2024-09-27 12:22:25 -0300

    .

--- main.js
+++ main.js
@@ -22,4 +22,7 @@ console.log(media(20,5,5))
 
 let nome = parseInt(prompt('qual é sua nota do primeiro trimestre?'))
 (prompt('qual é sua nota do segundo trimestre?'))
-(prompt('qual é sua nota do terceiro trimestre?'))
+
+(prompt('qual é sua nota do terceiro trimestre?')){
+    return()
+}
